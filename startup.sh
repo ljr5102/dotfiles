23f4c3b9b7b3dd8a7c72f04080cda5e28bea6a98
ljr5102 <ljr5102@gmail.com>
2017-12-09 14:16:00 -0500

    update startup script

--- startup.sh
+++ startup.sh
@@ -5,3 +5,12 @@ echo "Creating symlink for vimrc..."
 cd ~
 ln -s ~/code/dotfiles/vim/.vimrc ~/.vimrc
 echo "vimrc symlink created."
+
+echo "adding directories for vim backups"
+mkdir ~/.vim/backup//
+mkdir ~/.vim/swap//
+mkdir ~/.vim/undo//
+
+echo "adding the minpac package manager for vim"
+git clone https://github.com/k-takata/minpac.git \
+    ~/.vim/pack/minpac/opt/minpac
